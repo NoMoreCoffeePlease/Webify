8f1d31e0a331a360e8e422870f321aa1107ed652
Stoica Andrei <>
2020-07-03 14:53:55 +0300

    Added header

---
+++ web-build/precache-manifest.206bb5f83655ba246bfad507d21aab8b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "adedbfb8cdbb1752be9e",
+    "url": "/static/js/app.0f923026.chunk.js"
+  },
+  {
+    "revision": "3636aebd34579cc662c5",
+    "url": "/static/js/runtime~app.4b0f5a04.js"
+  },
+  {
+    "revision": "a3df1d2a03e36cb3e30a",
+    "url": "/static/js/2.dadba89a.chunk.js"
+  },
+  {
+    "revision": "c0f2633324e16c76413db4b581009d85",
+    "url": "/static/media/mug.c0f26333.png"
+  },
+  {
+    "revision": "f01adb5d7a9f6a42cd35ee06ca1771c3",
+    "url": "/static/media/me.f01adb5d.jpg"
+  },
+  {
+    "revision": "d6c50faa0726d9d6e447074f0dc3dde0",
+    "url": "/static/media/norbi_profile2.d6c50faa.jpg"
+  },
+  {
+    "revision": "9e93c0140bfb5c2c9582176206b1f604",
+    "url": "/static/media/paul_profile2.9e93c014.jpeg"
+  },
+  {
+    "revision": "742bbea93851e172b7fe1ebaf5ce8d88",
+    "url": "/static/media/idea_man.742bbea9.jpg"
+  },
+  {
+    "revision": "5a6b5d58fe0c3116739e6b581fa743d5",
+    "url": "/static/media/collaborate_group.5a6b5d58.jpg"
+  },
+  {
+    "revision": "fdad9c443adbdc661aa079202664f0b7",
+    "url": "/static/media/hands_together.fdad9c44.jpg"
+  },
+  {
+    "revision": "2d3b87292302e1b73543fd38b60b2544",
+    "url": "/static/media/shaking_hands.2d3b8729.png"
+  },
+  {
+    "revision": "c1dd6bbdad28e2305149d430adbd0498",
+    "url": "/static/media/background-coffee-laptop.c1dd6bbd.jpg"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "e809c7cefac9c924ce6c345c7f8df6b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd289eece27a83257ea72c85863c589f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  }
+];
